89271d00ca7ac1c9ed63ccd38eb9c2e11487126c
mrWhite <zackemannen81@gmail.com>
2025-09-28 12:02:44 +0200

    Published to itch.io , changed to relative pathes and fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  base: './',
   server: { port: 5173 },
   build: {
     sourcemap: true
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets', dest: 'src' },
+        { src: 'src/config', dest: 'src' }
+      ]
+    })
+  ]
 })
-
